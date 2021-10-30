22f8dc971bf35b9b0edcbd69fec703ccb4ca2289
J. Guilherme Coelho <jose.guilherme.oliveira@ccc.ufcg.edu.br>
2021-10-30 20:35:30 -0300

    Revert "mini-maxSum and Plusminus"

--- Mini-MaxSum.js
+++
@@ -1,6 +0,0 @@
-function miniMaxSum(arr) {
-  let min = arr.reduce((a, b) => a + b, 0) - Math.max.apply(Math, arr);
-  let max = arr.reduce((a, b) => a + b, 0) - Math.min.apply(Math, arr);
-
-  console.log(min, max);
-}
--- PlusMinus.js
+++
@@ -1,9 +0,0 @@
-function plusMinus(arr) {
-  let positives = arr.filter((num) => num > 0).length / arr.length;
-  let negatives = arr.filter((num) => num < 0).length / arr.length;
-  let zeros = arr.filter((num) => num == 0).length / arr.length;
-
-  console.log(
-    positives.toFixed(6) + '\n' + negatives.toFixed(6) + '\n' + zeros.toFixed(6)
-  );
-}
